894a541df697be4ef11feee937c729318a289519
garguidevsw <garguidevsw@gmail.com>
2023-04-22 17:36:06 -0600

    Add custom theme configuration

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.green,
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.orange,
+  Colors.teal,
+  Colors.black,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  })  : assert(selectedColor >= 0, 'selectedColor must be greater than 0'),
+        assert(selectedColor < colorList.length,
+            'selectedColor must be less or equal than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+      );
+}

9ca13c711d306f30da2efaadd214ecd9cd099253
garguidevsw <garguidevsw@gmail.com>
2023-04-28 22:46:23 -0600

    add basic routes

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -53,7 +54,7 @@ class _CustomListTile extends StatelessWidget {
         color: colors.primary,
       ),
       onTap: () {
-        //Todo: Navegar a otra pantalla
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
